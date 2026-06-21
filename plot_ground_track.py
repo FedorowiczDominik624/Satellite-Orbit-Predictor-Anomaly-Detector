f55d6a9292bbb33dadc0620a5d46c83b0bfbd414
Dominik Fedorowicz <dominik.fedorowicz12@gmail.com>
2026-06-21 15:19:17 +0900

    feat(plot): add ground track polylines over time window (wraparound TBD)

--- plot_ground_track.py
+++ plot_ground_track.py
@@ -1,9 +1,11 @@
 import json
+from propagate import propagate
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
+from datetime import datetime, timezone, timedelta
 
-def plot_ground_track_scatter(sats: list[dict]) -> None:
+def plot_ground_track_snapshot(sats: list[dict]) -> None:
     """Plot satellite ground-track positions on a PlateCarree world map.
 
     Draws all satellites as a scatter on a cartopy basemap with coastlines and
@@ -36,9 +38,56 @@ def plot_ground_track_scatter(sats: list[dict]) -> None:
     plt.savefig("docs/ground_track.png", bbox_inches="tight", dpi=150)
     plt.show()
 
+def plot_ground_track_lines(sats: list[dict], hours: float) -> None:
+    """plot ground-track polylines for each satellite over 'hours'
+    hours of orbit"""
 
+
+    fig, ax = plt.subplots(subplot_kw={"projection": ccrs.PlateCarree()})
+    ax.add_feature(cfeature.OCEAN)
+    ax.add_feature(cfeature.LAND)
+    ax.coastlines(linewidth=0.75)
+    ax.gridlines(draw_labels=True, alpha=0.4)
+
+    start = datetime.now(timezone.utc)
+    step = timedelta(minutes=5)
+
+    timestamps = []
+    for a in range(36):
+        timestamps.append(start + a * step)
+
+    all_lats = []
+    all_lons = []
+
+    for  sat in sats:
+        this_sat_lats = []
+        this_sat_lons = []
+        for t in timestamps:
+            lat, lons, _ = propagate(sat, t)
+            this_sat_lats.append(lat)
+            this_sat_lons.append(lons)
+        all_lats.append(this_sat_lats)
+        all_lons.append(this_sat_lons)
+
+
+    for i in range(len(all_lons)):
+        all_xs = all_lons[i]
+        all_ys = all_lats[i]
+        ax.plot(all_xs, all_ys, transform=ccrs.PlateCarree())
+
+    ax.set_title("Satellite ground track over 3 hours")
+    ax.set_xlabel("Longitude")
+    ax.set_ylabel("Latitude")
+    plt.savefig("docs/ground_track_lines.png", bbox_inches="tight", dpi=150)
+    plt.show()
 
 if __name__ == "__main__":
+    # Load propagated sats for the snapshot
     with open("data/propagated_satellites.json") as f:
-        satellites = json.load(f)
-    plot_ground_track_scatter(satellites)
+        propagated_sats = json.load(f)
+    plot_ground_track_snapshot(propagated_sats)
+    
+    # Load raw sats for the lines version
+    with open("data/satellites.json") as f:
+        raw_sats = json.load(f)
+    plot_ground_track_lines(raw_sats, hours=3.0)
